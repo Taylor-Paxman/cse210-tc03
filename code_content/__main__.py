7e77a06099d4d8c24502f1379f30f43f7f0478f4
ArcaneTechno <noah.james.blevins@gmail.com>
2021-09-22 22:34:41 -0600

    Added "if main then run main"

--- code_content/__main__.py
+++ code_content/__main__.py
@@ -1,7 +1,10 @@
 from blevins import  get_blevins_name
 
-print('Welcome to our collaborative program')
 
-get_blevins_name()
+def main():
+    print('Welcome to our collaborative program')
 
+    get_blevins_name()
 
+if __name__ == "__main__":
+    main()
