e5a1d67fe989b9110f0d37382cd9421491f48c36
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2021-10-01 21:05:13 +0200

    Update __main__.py
    Hilo game

--- code_content/__main__.py
+++ code_content/__main__.py
@@ -1,6 +1,7 @@
 from marcus import  get_marcus_name
 from blevins import  get_blevins_name
 from taylor import get_taylor_name
+from owamamwen import get_owamamwen_hilo
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     get_marcus_name()
     get_blevins_name()
     get_taylor_name()
+    get_owamamwen_hilo()
 
 if __name__ == "__main__":
     main()
